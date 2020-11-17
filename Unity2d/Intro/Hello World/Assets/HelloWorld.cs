b82c48df30d5c6d51ec97397bd1177fe69848b4c
Chris Maillefaud <chrismaillefaud@gmail.com>
2020-11-17 17:15:55 -0300

    chore: Add existing local code to Github

---
+++ Unity2d/Intro/Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerName = "Chris";
+        print($"Hello {PlayerName}");
+    }
+
+    public string PlayerName { get; set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity2d/NumberWizard/Number Wizard Console/Assets/Scripts/Tests/NumberWizardTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NumberWizardTests
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestMinimumandMaximumValuesPasses()
+        {
+            // Arrange
+            var numberWizard = new GameObject("wizard").AddComponent<NumberWizard>();
+            
+            // Act
+            numberWizard.StartGame();
+
+            // Assert
+            Assert.IsTrue(numberWizard.minNumber == 1);
+            Assert.IsTrue(numberWizard.maxNumber == 1001);
+            
+            // Finish
+            yield return null;
+        }
+    }
+}
