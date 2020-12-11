0b11ca463af8c3a6f23a835943a7bb5bfd6d1439
Chris Maillefaud <chrismaillefaud@gmail.com>
2020-12-11 12:11:02 -0300

    * Finished Number Wizard * Start Block Breaker

---
+++ Unity2d/Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Paddle paddle1;
+
+    private Vector2 _paddleToBallVector2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _paddleToBallVector2 = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentPaddlePosition = paddle1.transform.position;
+        var paddlePosition = new Vector2(currentPaddlePosition.x, currentPaddlePosition.y);
+        transform.position = paddlePosition + _paddleToBallVector2;
+    }
+}
---
+++ Unity2d/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    Camera camera => Camera.main;
+    
+    float WorldUnits => (camera.orthographicSize * 2) * camera.aspect;
+
+    private float MAXPaddlePosition => (WorldUnits / 2) - 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentPosition = transform.position;
+
+        var newPositionX = Input.mousePosition.x / Screen.width * WorldUnits;
+
+        var paddlePosition = new Vector2(currentPosition.x, currentPosition.y);
+
+        paddlePosition.x = Mathf.Clamp(newPositionX - (WorldUnits / 2), -MAXPaddlePosition, MAXPaddlePosition);
+
+        transform.position = paddlePosition;
+    }
+}
